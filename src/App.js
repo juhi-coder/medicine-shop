4dc04cc30484450f36aeca0c93556e33f72c23ff
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-11 14:10:01 +0530

    Admin panel for Medicine shop Sellers with stock quantity management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import MedicalGetItem from './components/MedicalGetItem';
+import MedicalForm from './components/MedicalForm';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <MedicalForm></MedicalForm>
+    <MedicalGetItem></MedicalGetItem>
+   </>
   );
 }
 
